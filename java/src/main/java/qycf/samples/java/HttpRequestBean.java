667309ec64865b0aadd2d3d7820d19db087021c7
harrison <haishengbai@gmail.com>
2021-01-06 08:35:27 +0800

    qycf samples init

---
+++ java/src/main/java/qycf/samples/java/HttpRequestBean.java
@@ -0,0 +1,43 @@
+package qycf.samples.java;
+
+
+public class HttpRequestBean {
+
+    private String aggregationId;
+    private Integer identifyTopNum;
+    private String image;
+    private Integer minScore;
+
+
+    public String getAggregationId() {
+        return aggregationId;
+    }
+
+    public void setAggregationId(String aggregationId) {
+        this.aggregationId = aggregationId;
+    }
+
+    public Integer getIdentifyTopNum() {
+        return identifyTopNum;
+    }
+
+    public void setIdentifyTopNum(Integer identifyTopNum) {
+        this.identifyTopNum = identifyTopNum;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getMinScore() {
+        return minScore;
+    }
+
+    public void setMinScore(Integer minScore) {
+        this.minScore = minScore;
+    }
+}
